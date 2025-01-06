915dadf7dd29f65d57ff9b16a472e7284d7f0b51
Hendrik du Toit <hendrik@brightedge.co.za>
2025-01-06 18:59:56 +0200

    Restucture and configure Poetry

--- src/csvwrpr.py
+++ src/csvwrpr.py
@@ -2,14 +2,16 @@
 
 Insert description here
 """
+
 import os
 import sys
+from pathlib import Path
+
+import displayfx
+from beetools import msg as tls_msg
+from beetools import utils as tls_utils
 
 # import logging
-from pathlib import Path
-import displayfx
-from beetools import utils as bu
-from beetools import msg as bm
 
 _path = Path(sys.argv[0])
 _name = _path.stem
@@ -27,20 +29,20 @@ class CsvWrpr:
     def __init__(
         self,
         p_csv_file_name,
-        p_key1='',
-        p_key2='',
-        p_data_delimiter='',
-        p_header_delimiter='',
-        p_header='',
+        p_key1="",
+        p_key2="",
+        p_data_delimiter="",
+        p_header_delimiter="",
+        p_header="",
         p_del_head=False,
         p_struc_type={},
-        p_csv_corr_str_file_name='',
+        p_csv_corr_str_file_name="",
         p_replace_header=False,
         p_subset_range=[],
         p_verbose=False,
         p_convert_none=True,
-        p_bar_len=bu.BAR_LEN,
-        p_msg_width=bu.MSG_LEN,
+        p_bar_len=tls_utils.BAR_LEN,
+        p_msg_width=tls_utils.MSG_LEN,
         p_match_nr_of_fields=False,
     ):
         """Initialize the class
@@ -117,15 +119,15 @@ class CsvWrpr:
         self.success = False
         self.bar_len = p_bar_len
         self.msg_width = p_msg_width
-        self.combined_field = ''
+        self.combined_field = ""
         self.convert_none = p_convert_none
         self.corr_str_list = []
         self.csv_corr_str_file_name = p_csv_corr_str_file_name
         self.csv_file_fame = p_csv_file_name
         self.data_delimiter = p_data_delimiter
         self.del_head = p_del_head
-        self.delimiter_list = [',', ';', '~', '|']
-        self.head = ''
+        self.delimiter_list = [",", ";", "~", "|"]
+        self.head = ""
         self.header = p_header
         self.header_delimiter = p_header_delimiter
         self.key1 = p_key1
@@ -136,11 +138,11 @@ class CsvWrpr:
         self.silent = p_verbose
         self.struc_type = p_struc_type
         self.subset_range = p_subset_range
-        self.tail = ''
-        self.t_tow = ''
+        self.tail = ""
+        self.t_tow = ""
         self.read_csv_corr_str_file()
         (self.head, self.tail) = os.path.split(self.csv_file_fame)
-        if self.key2 == '':
+        if self.key2 == "":
             if self.struc_type == [] or self.struc_type == ():
                 self.csv_db = []
             else:
@@ -157,17 +159,17 @@ class CsvWrpr:
     def append(
         self,
         p_append_path,
-        p_key1='',
-        p_key2='',
-        p_data_delimiter='',
-        p_header_delimiter='',
-        p_header='',
+        p_key1="",
+        p_key2="",
+        p_data_delimiter="",
+        p_header_delimiter="",
+        p_header="",
         p_del_head=False,
-        p_csv_corr_str_file_name='',
+        p_csv_corr_str_file_name="",
         p_replace_header=False,
         p_subset_range=[],
     ):
-        """Apend another csv file to the current data.
+        """Append another csv file to the current data.
 
         Parameters
          - p_append_path
@@ -244,14 +246,12 @@ class CsvWrpr:
 
     # end append
 
-    def export(self, p_export_file_name, p_export_delimiter='|'):
+    def export(self, p_export_file_name, p_export_delimiter="|"):
         """Export all of the current records."""
         if isinstance(self.csv_db, (list, tuple)):
-            export_file = open(
-                p_export_file_name, 'w+', encoding='utf-8', errors='ignore'
-            )
+            export_file = open(p_export_file_name, "w+", encoding="utf-8", errors="ignore")
             for i in range(len(self.csv_db)):
-                export_str = p_export_delimiter.join(map(str, self.csv_db[i])) + '\n'
+                export_str = p_export_delimiter.join(map(str, self.csv_db[i])) + "\n"
                 export_file.write(export_str)
             export_file.close()
         elif isinstance(self.csv_db, dict):
@@ -262,19 +262,15 @@ class CsvWrpr:
 
     # end export
 
-    def export_sub_set(
-        self, p_export_file_name, p_sub_set_list, p_export_delimiter='|'
-    ):
+    def export_sub_set(self, p_export_file_name, p_sub_set_list, p_export_delimiter="|"):
         """Description"""
-        export_file = open(p_export_file_name, 'w+', encoding='utf-8', errors='ignore')
-        export_str = p_export_delimiter.join(map(str, self.header)) + '\n'
+        export_file = open(p_export_file_name, "w+", encoding="utf-8", errors="ignore")
+        export_str = p_export_delimiter.join(map(str, self.header)) + "\n"
         export_file.write(export_str)
         list_len = len(p_sub_set_list)
         export_qty = 0
-        msg = bm.display(
-            'Writing subset {} ({})'.format(
-                os.path.split(p_export_file_name)[1], list_len
-            ),
+        msg = tls_msg.display(
+            f"Writing subset {os.path.split(p_export_file_name)[1]} ({list_len})",
             p_len=self.msg_width,
         )
         dfx = displayfx.DisplayFx(
@@ -291,9 +287,7 @@ class CsvWrpr:
                 # in a list of tuple scenario.
                 for csv_db_row in self.csv_db:
                     if sub_set_id == csv_db_row[field_pos]:
-                        export_str = (
-                            p_export_delimiter.join(map(str, csv_db_row)) + '\n'
-                        )
+                        export_str = p_export_delimiter.join(map(str, csv_db_row)) + "\n"
                         export_file.write(export_str)
                         export_qty += 1
                 dfx.update(dfx_cntr)
@@ -303,9 +297,7 @@ class CsvWrpr:
                 if sub_set_id in self.csv_db:
                     for field in self.header:
                         export_tow.append(self.csv_db[sub_set_id][field])
-                    export_file.write(
-                        p_export_delimiter.join(map(str, export_tow)) + '\n'
-                    )
+                    export_file.write(p_export_delimiter.join(map(str, export_tow)) + "\n")
                     export_qty += 1
                 dfx.update(dfx_cntr)
         export_file.close()
@@ -316,13 +308,11 @@ class CsvWrpr:
     def read_csv_corr_str_file(self):
         """Parameters"""
         if self.csv_corr_str_file_name:
-            corr_file = open(
-                self.csv_corr_str_file_name, 'r', encoding='utf-8', errors='ignore'
-            )
+            corr_file = open(self.csv_corr_str_file_name, encoding="utf-8", errors="ignore")
             raw_corr_data = corr_file.readlines()
             corr_file.close()
             for row in raw_corr_data:
-                self.corr_str_list.append(row[:-1].split('~'))
+                self.corr_str_list.append(row[:-1].split("~"))
 
     # end read_csv_corr_str_file
 
@@ -352,7 +342,7 @@ class CsvWrpr:
             row = replace_contents(p_row)
             row = adjust_delimiters(row)
             if not self.t_tow:
-                self.combined_field = ''
+                self.combined_field = ""
                 new_row = []
             else:
                 new_row = self.t_tow
@@ -360,7 +350,7 @@ class CsvWrpr:
             for i, field in enumerate(row):
                 if self.combined_field:
                     field = self.combined_field + self.data_delimiter + field
-                    self.combined_field = ''
+                    self.combined_field = ""
                 if field[:1] == '"':
                     if field[-1:] == '"' and len(field) > 1:
                         new_row.append(field[1:-1])
@@ -370,29 +360,24 @@ class CsvWrpr:
                     new_row.append(field)
             if self.convert_none:
                 for i, field in enumerate(new_row):
-                    if field in ['NULL', 'None']:
+                    if field in ["NULL", "None"]:
                         new_row[i] = None
             if self.combined_field:
                 self.t_tow = new_row
             else:
-                self.t_tow = ''
+                self.t_tow = ""
             return new_row
 
         # end fix_row
 
         def get_delimiter():
             """Description"""
-            if self.data_delimiter == '':
+            if self.data_delimiter == "":
                 delimiter_cntr = 0
                 delimiter_pos = 0
                 for i, delimiter in enumerate(self.delimiter_list):
-                    if (
-                        str(raw_csv_file_data[0]).count(self.delimiter_list[i])
-                        > delimiter_cntr
-                    ):
-                        delimiter_cntr = str(raw_csv_file_data[0]).count(
-                            self.delimiter_list[i]
-                        )
+                    if str(raw_csv_file_data[0]).count(self.delimiter_list[i]) > delimiter_cntr:
+                        delimiter_cntr = str(raw_csv_file_data[0]).count(self.delimiter_list[i])
                         delimiter_pos = i
                 self.data_delimiter = self.delimiter_list[delimiter_pos]
                 if not self.header_delimiter:
@@ -443,8 +428,7 @@ class CsvWrpr:
                 csv_row[field] = p_row[j]
             if csv_row[self.key1].isnumeric():
                 if not self.subset_range or (
-                    int(csv_row[self.key1]) >= self.subset_range[0]
-                    and int(csv_row[self.key1]) <= self.subset_range[1]
+                    int(csv_row[self.key1]) >= self.subset_range[0] and int(csv_row[self.key1]) <= self.subset_range[1]
                 ):
                     self.csv_db[csv_row[self.key1]] = csv_row
             else:
@@ -468,12 +452,7 @@ class CsvWrpr:
         def replace_header():
             """Replace the header according to parameter switch"""
             if not self.header:
-                self.header = (
-                    raw_csv_file_data[0]
-                    .rstrip('\n')
-                    .replace('"', '')
-                    .split(self.data_delimiter)
-                )
+                self.header = raw_csv_file_data[0].rstrip("\n").replace('"', "").split(self.data_delimiter)
             if self.replace_header:
                 if self.struc_type == []:
                     self.csv_db.append(self.header)
@@ -483,12 +462,7 @@ class CsvWrpr:
                     self.csv_db[self.key1] = dict(
                         zip(
                             self.header,
-                            [
-                                x.strip('\n"')
-                                for x in raw_csv_file_data[0].split(
-                                    self.header_delimiter
-                                )
-                            ],
+                            [x.strip('\n"') for x in raw_csv_file_data[0].split(self.header_delimiter)],
                         )
                     )
                 del raw_csv_file_data[0]
@@ -498,7 +472,7 @@ class CsvWrpr:
         # end replace_header
 
         if os.path.isfile(self.csv_file_fame):
-            csv_file = open(self.csv_file_fame, 'r', encoding='utf-8', errors='ignore')
+            csv_file = open(self.csv_file_fame, encoding="utf-8", errors="ignore")
             raw_csv_file_data = csv_file.readlines()
             csv_file.close()
             self.nr_of_rows = len(raw_csv_file_data)
@@ -511,10 +485,8 @@ class CsvWrpr:
                     key1_index = self.header.index(self.key1)
                 else:
                     key1_index = None
-                msg = bm.display(
-                    'Reading {} ({})'.format(
-                        os.path.split(self.csv_file_fame)[1], self.nr_of_rows
-                    ),
+                msg = tls_msg.display(
+                    f"Reading {os.path.split(self.csv_file_fame)[1]} ({self.nr_of_rows})",
                     p_len=self.msg_width,
                 )
                 dfx = displayfx.DisplayFx(
@@ -524,11 +496,9 @@ class CsvWrpr:
                     p_bar_len=self.bar_len,
                 )
                 for row_cntr, row in enumerate(raw_csv_file_data):
-                    fixed_row = fix_row(row.rstrip('\n'))
+                    fixed_row = fix_row(row.rstrip("\n"))
                     if not self.t_tow:
-                        if (
-                            self.match_nr_of_fields and header_len == len(fixed_row)
-                        ) or not self.match_nr_of_fields:
+                        if (self.match_nr_of_fields and header_len == len(fixed_row)) or not self.match_nr_of_fields:
                             if self.struc_type == []:
                                 append_row_to_list(fixed_row)
                             elif self.struc_type == ():
@@ -541,19 +511,19 @@ class CsvWrpr:
                                 len(fixed_row),
                                 row_cntr,
                                 self.csv_file_fame,
-                                ','.join(self.header),
+                                ",".join(self.header),
                                 row,
-                                ','.join(fixed_row),
+                                ",".join(fixed_row),
                             )
                     dfx.update(row_cntr)
                 self.success = True
             else:
                 if not self.silent:
-                    log_str = 'File does not have data - {}'.format(self.csv_file_fame)
+                    log_str = f"File does not have data - {self.csv_file_fame}"
             del_header()
         else:
             if not self.silent:
-                log_str = 'File does not exist: {}\n'.format(self.csv_file_fame)
+                log_str = f"File does not exist: {self.csv_file_fame}\n"
                 print(log_str)
         return self.csv_db
 
@@ -563,7 +533,7 @@ class CsvWrpr:
         """Description"""
         log_str = self.read_one_key_csv()
         self.csv_db = {}
-        msg = bm.display('Build two key structure', p_len=self.msg_width)
+        msg = tls_msg.display("Build two key structure", p_len=self.msg_width)
         dfx = displayfx.DisplayFx(
             len(log_str),
             p_msg=msg,
@@ -577,12 +547,12 @@ class CsvWrpr:
             if row_dict[self.key1] not in self.csv_db:
                 self.csv_db[row_dict[self.key1]] = {row_dict[self.key2]: row_dict}
             else:
-                # Cater for duplicate keys.  Asume the last entry is the incorrect one.
+                # Cater for duplicate keys.  Assume the last entry is the incorrect one.
                 if not row_dict[self.key2] in self.csv_db[row_dict[self.key1]]:
                     self.csv_db[row_dict[self.key1]][row_dict[self.key2]] = row_dict
                 else:
-                    # Log duplicates.  Insert wrting errors to file here if necessary.
-                    error_str = 'read_two_key_csv Duplicate entry: %s %s' % (
+                    # Log duplicates.  Insert writing errors to file here if necessary.
+                    error_str = "read_two_key_csv Duplicate entry: {} {}".format(
                         row_dict[self.key1],
                         row_dict[self.key2],
                     )
